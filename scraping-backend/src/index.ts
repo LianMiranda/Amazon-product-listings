aeca91772c2a50ce4382f2f96a565347e8481971
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-04-22 19:42:05 -0400

    fix: solving cors problem

--- scraping-backend/src/index.ts
+++ scraping-backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes/scrape.route";
 
 export const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => {
